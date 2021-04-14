4e2edae3add647376fa95b403c5ad11a65463bd3
Woerdie <Daan.woerdman@gmail.com>
2021-04-14 23:42:46 +0200

    Add files via upload

---
+++ admin/toevoegen.php
@@ -0,0 +1,28 @@
+<?php
+// db connect
+$servername = "localhost";
+$username = "Bas";
+$password = "spaarSpook9";
+$dbname = "winkelcentrum";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    writeMsg("verbinden mislukt, probeer het later nog eens");
+    die();
+}
+// Prijs aanmaken
+
+$winkID = mysqli_real_escape_string($conn, $_POST['winkID']);
+$prize = mysqli_real_escape_string($conn, $_POST['prize']);
+
+$stmt = $conn->prepare("INSERT INTO prijzen (WinkelID,PrijsNaam ) VALUES (?, ?)");
+
+
+$stmt->bind_param('is', $winkID,$prize);
+$stmt->execute();
+
+echo "<script>
+      window.location.replace(\"prijsaanmaken.html\");
+    </script>";
+die();
